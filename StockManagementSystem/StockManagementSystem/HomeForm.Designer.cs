c5618563af6d23d7e685b9f897e2f51fb18d6c97
raihanM95 <mahmudrony95@gmail.com>
2019-07-06 00:27:03 +0600

    index image add

--- StockManagementSystem/StockManagementSystem/HomeForm.Designer.cs
+++ StockManagementSystem/StockManagementSystem/HomeForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomeForm));
             this.menuStrip = new System.Windows.Forms.MenuStrip();
             this.CategoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.CategorySetupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -46,8 +47,10 @@
             this.toolTip = new System.Windows.Forms.ToolTip(this.components);
             this.clock = new System.Windows.Forms.Label();
             this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.menuStrip.SuspendLayout();
             this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip
@@ -69,12 +72,14 @@
             // 
             this.CategoryToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.CategorySetupToolStripMenuItem});
+            this.CategoryToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("CategoryToolStripMenuItem.Image")));
             this.CategoryToolStripMenuItem.Name = "CategoryToolStripMenuItem";
-            this.CategoryToolStripMenuItem.Size = new System.Drawing.Size(96, 29);
+            this.CategoryToolStripMenuItem.Size = new System.Drawing.Size(120, 29);
             this.CategoryToolStripMenuItem.Text = "Category";
             // 
             // CategorySetupToolStripMenuItem
             // 
+            this.CategorySetupToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("CategorySetupToolStripMenuItem.Image")));
             this.CategorySetupToolStripMenuItem.Name = "CategorySetupToolStripMenuItem";
             this.CategorySetupToolStripMenuItem.Size = new System.Drawing.Size(219, 30);
             this.CategorySetupToolStripMenuItem.Text = "Category Setup";
@@ -84,12 +89,14 @@
             // 
             this.CompanyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.CompanySetupToolStripMenuItem});
+            this.CompanyToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("CompanyToolStripMenuItem.Image")));
             this.CompanyToolStripMenuItem.Name = "CompanyToolStripMenuItem";
-            this.CompanyToolStripMenuItem.Size = new System.Drawing.Size(101, 29);
+            this.CompanyToolStripMenuItem.Size = new System.Drawing.Size(125, 29);
             this.CompanyToolStripMenuItem.Text = "Company";
             // 
             // CompanySetupToolStripMenuItem
             // 
+            this.CompanySetupToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("CompanySetupToolStripMenuItem.Image")));
             this.CompanySetupToolStripMenuItem.Name = "CompanySetupToolStripMenuItem";
             this.CompanySetupToolStripMenuItem.Size = new System.Drawing.Size(224, 30);
             this.CompanySetupToolStripMenuItem.Text = "Company Setup";
@@ -100,12 +107,14 @@
             this.ItemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.ItemSetupToolStripMenuItem,
             this.ViewItemsToolStripMenuItem});
+            this.ItemToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ItemToolStripMenuItem.Image")));
             this.ItemToolStripMenuItem.Name = "ItemToolStripMenuItem";
-            this.ItemToolStripMenuItem.Size = new System.Drawing.Size(60, 29);
+            this.ItemToolStripMenuItem.Size = new System.Drawing.Size(84, 29);
             this.ItemToolStripMenuItem.Text = "Item";
             // 
             // ItemSetupToolStripMenuItem
             // 
+            this.ItemSetupToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ItemSetupToolStripMenuItem.Image")));
             this.ItemSetupToolStripMenuItem.Name = "ItemSetupToolStripMenuItem";
             this.ItemSetupToolStripMenuItem.Size = new System.Drawing.Size(183, 30);
             this.ItemSetupToolStripMenuItem.Text = "Item Setup";
@@ -113,6 +122,7 @@
             // 
             // ViewItemsToolStripMenuItem
             // 
+            this.ViewItemsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ViewItemsToolStripMenuItem.Image")));
             this.ViewItemsToolStripMenuItem.Name = "ViewItemsToolStripMenuItem";
             this.ViewItemsToolStripMenuItem.Size = new System.Drawing.Size(183, 30);
             this.ViewItemsToolStripMenuItem.Text = "View Items";
@@ -124,12 +134,14 @@
             this.StockInToolStripMenuItem,
             this.StockOutToolStripMenuItem,
             this.ViewReportToolStripMenuItem});
+            this.StockToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("StockToolStripMenuItem.Image")));
             this.StockToolStripMenuItem.Name = "StockToolStripMenuItem";
-            this.StockToolStripMenuItem.Size = new System.Drawing.Size(67, 29);
+            this.StockToolStripMenuItem.Size = new System.Drawing.Size(91, 29);
             this.StockToolStripMenuItem.Text = "Stock";
             // 
             // StockInToolStripMenuItem
             // 
+            this.StockInToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("StockInToolStripMenuItem.Image")));
             this.StockInToolStripMenuItem.Name = "StockInToolStripMenuItem";
             this.StockInToolStripMenuItem.Size = new System.Drawing.Size(191, 30);
             this.StockInToolStripMenuItem.Text = "Stock In";
@@ -137,6 +149,7 @@
             // 
             // StockOutToolStripMenuItem
             // 
+            this.StockOutToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("StockOutToolStripMenuItem.Image")));
             this.StockOutToolStripMenuItem.Name = "StockOutToolStripMenuItem";
             this.StockOutToolStripMenuItem.Size = new System.Drawing.Size(191, 30);
             this.StockOutToolStripMenuItem.Text = "Stock Out";
@@ -144,6 +157,7 @@
             // 
             // ViewReportToolStripMenuItem
             // 
+            this.ViewReportToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ViewReportToolStripMenuItem.Image")));
             this.ViewReportToolStripMenuItem.Name = "ViewReportToolStripMenuItem";
             this.ViewReportToolStripMenuItem.Size = new System.Drawing.Size(191, 30);
             this.ViewReportToolStripMenuItem.Text = "View Report";
@@ -185,11 +199,24 @@
             this.Timer.Interval = 1000;
             this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 35);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1064, 632);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
             // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1064, 697);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.clock);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.menuStrip);
@@ -204,6 +231,7 @@
             this.menuStrip.PerformLayout();
             this.statusStrip.ResumeLayout(false);
             this.statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,6 +256,7 @@
         private System.Windows.Forms.ToolStripMenuItem ViewReportToolStripMenuItem;
         private System.Windows.Forms.Label clock;
         private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
